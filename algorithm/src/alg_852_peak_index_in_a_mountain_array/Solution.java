e96c39735b101ee8b271a95657e8068fe05ff512
justin.tsui <justin0081@163.com>
2018-11-06 15:59:33 +0800

    852 peak index in a mountain array

---
+++ algorithm/src/alg_852_peak_index_in_a_mountain_array/Solution.java
@@ -0,0 +1,18 @@
+package alg_852_peak_index_in_a_mountain_array;
+
+/**
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ */
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
