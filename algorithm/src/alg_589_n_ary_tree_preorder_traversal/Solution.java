5a8880a547fdde1f6ae4f18ee3685b468f3d96ab
justin.tsui <justin0081@163.com>
2018-11-07 14:29:53 +0800

    589 n-ary tree preorder traversal

---
+++ algorithm/src/alg_589_n_ary_tree_preorder_traversal/Solution.java
@@ -0,0 +1,65 @@
+package alg_589_n_ary_tree_preorder_traversal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+ */
+class Solution {
+    public List<Integer> preorder_iterative(Node root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        Stack<Node> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+
+            result.add(node.val);
+            if (node.children != null) {
+                for (int i = node.children.size() - 1; i >= 0; i--) {
+                    stack.push(node.children.get(i));
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> preorder_recursive(Node root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result = travel(result, root);
+        return result;
+    }
+
+    private List<Integer> travel(List<Integer> list, Node node) {
+        list.add(node.val);
+        if (node.children != null) {
+            for (Node child : node.children) {
+                list = travel(list, child);
+            }
+        }
+        return list;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
