6be8221463929e636e0ae58f94180576c58a3344
justin.tsui <justin0081@163.com>
2018-11-10 18:17:52 +0800

    700 search in a binary tree

---
+++ algorithm/src/alg_700_search_in_a_binary_tree/Solution.java
@@ -0,0 +1,33 @@
+package alg_700_search_in_a_binary_tree;
+
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ */
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+}
