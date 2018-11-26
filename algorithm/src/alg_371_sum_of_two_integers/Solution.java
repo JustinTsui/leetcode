a4e64b76fb5aad56f007027c5d1cd332384a07b6
justin.tsui <justin0081@163.com>
2018-11-26 23:28:26 +0800

    371 sum of two integers

---
+++ algorithm/src/alg_371_sum_of_two_integers/Solution.java
@@ -0,0 +1,10 @@
+package alg_371_sum_of_two_integers;
+
+/**
+ * https://leetcode.com/problems/sum-of-two-integers/
+ */
+public class Solution {
+    public int getSum(int a, int b) {
+        return Math.addExact(a, b);
+    }
+}
