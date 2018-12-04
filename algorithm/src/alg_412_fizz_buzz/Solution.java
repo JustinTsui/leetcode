96275dff2393ae58b86e3504872a6d1691115565
justin.tsui <justin0081@163.com>
2018-12-04 19:20:52 +0800

    412 fizz buzz

---
+++ algorithm/src/alg_412_fizz_buzz/Solution.java
@@ -0,0 +1,29 @@
+package alg_412_fizz_buzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/fizz-buzz/
+ */
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>(0);
+
+        for (int i = 1; i <= n; i++) {
+            int mod3 = i % 3;
+            int mod5 = i % 5;
+            if (mod3 == 0 && mod5 == 0) {
+                result.add("FizzBuzz");
+            } else if (mod3 == 0) {
+                result.add("Fizz");
+            } else if (mod5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(Integer.toString(i));
+            }
+
+        }
+        return result;
+    }
+}
