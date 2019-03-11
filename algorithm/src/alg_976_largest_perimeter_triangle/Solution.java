777eef02aa607898d170310b712cea7761ed6424
justin.tsui <justin0081@163.com>
2019-03-11 16:23:32 +0800

    976 largest perimeter triangle

---
+++ algorithm/src/alg_976_largest_perimeter_triangle/Solution.java
@@ -0,0 +1,18 @@
+package alg_976_largest_perimeter_triangle;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/largest-perimeter-triangle/
+ */
+public class Solution {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length - 1; i >= 2; i--) {
+            if (A[i] < A[i - 1] + A[i - 2]) {
+                return A[i] + A[i - 1] + A[i - 2];
+            }
+        }
+        return 0;
+    }
+}
