93b71ffd17e78ab0b96f70c91192907c7807eb82
JustinTsui <justin0081@163.com>
2019-12-03 19:24:35 +0800

    938. Range Sum of BST

---
+++ algorithms/src/test/java/ps_0901_1000/alg_938_range_sum_of_bst/SolutionTest.java
@@ -0,0 +1,42 @@
+package ps_0901_1000.alg_938_range_sum_of_bst;
+
+import common.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    private Solution solution = new Solution();
+
+    @Test
+    public void testCase1() {
+        TreeNode root = new TreeNode(10);
+
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+
+        root.right = new TreeNode(15);
+        root.right.right = new TreeNode(18);
+
+        assertEquals(32, solution.rangeSumBST(root, 7, 15));
+    }
+
+    @Test
+    public void testCase2() {
+        TreeNode root = new TreeNode(10);
+
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(7);
+        root.left.right.left = new TreeNode(6);
+
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(18);
+
+        assertEquals(23, solution.rangeSumBST(root, 6, 10));
+    }
+}
